fd57fc7f98cc123887b76f1a275054b504cceb91
Chris Wilcoxson <chris@slushman.com>
2022-02-26 10:25:55 -0700

    Commit footer to repo.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import Logo from '../Logo';
+
+const Footer = () => {
+
+  return (
+    <footer className="flex justify-center gap-8 p-8 content-center items-baseline">
+      &copy; {new Date().getFullYear()}<a className="inline ml-[-24px]" href="https://www.slushman.com"><Logo /></a>
+    </footer>
+  );
+};
+
+export default React.memo(Footer);
