6f7948ffdf4b43336772f6ece01e8e626cf345e6
Chris Wilcoxson <chris@slushman.com>
2022-03-25 17:02:57 -0600

    Trim up more code.

--- src/components/WordList/WordList.tsx
+++ src/components/WordList/WordList.tsx
@@ -34,43 +34,15 @@ const WordList = ({
       return true;
     }
 
-    let hasAbsents = false;
-    let hasPresents = true;
-    let hasFirst = true;
-    let hasSecond = true;
-    let hasThird = true;
-    let hasFourth = true;
-    let hasFifth = true;
-
     const letterArray = Array.from(word.toUpperCase());
 
-    if (absentList.length > 0) {
-      hasAbsents = letterArray.some(letter => absentList.includes(letter));
-    }
-
-    if (presentList.length > 0) {
-      hasPresents = presentList.every(letter => letterArray.includes(letter));
-    }
-
-    if (firstLetter !== '') {
-      hasFirst = firstLetter === letterArray[0];
-    }
-
-    if (secondLetter !== '') {
-      hasSecond = secondLetter === letterArray[1];
-    }
-
-    if (thirdLetter !== '') {
-      hasThird = thirdLetter === letterArray[2];
-    }
-
-    if (fourthLetter !== '') {
-      hasFourth = fourthLetter === letterArray[3];
-    }
-
-    if (fifthLetter !== '') {
-      hasFifth = fifthLetter === letterArray[4];
-    }
+    const hasAbsents = absentList.length > 0 && letterArray.some(letter => absentList.includes(letter));
+    const hasPresents = presentList.length > 0 && presentList.every(letter => letterArray.includes(letter));
+    const hasFirst = firstLetter !== '' && firstLetter === letterArray[0];
+    const hasSecond = secondLetter !== '' && secondLetter === letterArray[1];
+    const hasThird = thirdLetter !== '' && thirdLetter === letterArray[2];
+    const hasFourth = fourthLetter !== '' && fourthLetter === letterArray[3];
+    const hasFifth = fifthLetter !== '' && fifthLetter === letterArray[4];
 
     return !hasAbsents && hasPresents && hasFirst && hasSecond && hasThird && hasFourth && hasFifth;
   });
